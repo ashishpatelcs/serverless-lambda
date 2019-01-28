e608a9edf5e4f05dba5922094fad8db31ad3dea0
Ashish Patel <ashishpatel@skaas.guru>
2019-01-28 13:04:15 +0530

    added files and pdfs folder

--- handler.js
+++ handler.js
@@ -4,8 +4,19 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'Server is up and running! '
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.generatePDF = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'PDF generated! '
     }),
   };
 
